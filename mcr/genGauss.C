70bdf9a35a14e668b920afffc1c44233e755d94d
gabouric <gabouric@gmail.com>
2022-03-14 22:04:28 +0100

    Fixed empty histogram

--- mcr/genGauss.C
+++ mcr/genGauss.C
@@ -4,14 +4,18 @@
 void genGauss()
 {
     TH1F* h = new TH1F("hGauss", "Gaussian distribution", 100, -2., 2.);
-    TF1* gaus = new TF1("gaus", "gaus", -2., 2.);
+    h->SetStats(0);
+    TF1* gaus = new TF1("gaus", "gaus(0)", -2., 2.);
+    gaus->SetParameters(1., 0., .5);
 
     // Draw 10000 numbers according the gaussian law
     // We use gRandom->Uniform(MAX)  to draw numbers
     // uniformly  between  0. and MAX
+
+    Float_t x, y;
     for (Int_t i=0; i<10000; i++) {
-        Float_t x = gRandom->Uniform(4.) - 2.;
-        Float_t y = gRandom->Uniform(gaus->Eval(0));
+        x = gRandom->Uniform(4.) - 2.;
+        y = gRandom->Uniform(gaus->Eval(0));
 
         if (y <= gaus->Eval(x)) {
             h->Fill(x);
