a4dcb7f75af2b394d479d3de64bd2683c94b6204
gabouric <gabouric@gmail.com>
2022-03-16 11:10:25 +0100

    flie created

---
+++ src/ShowerConstants.h
@@ -0,0 +1,17 @@
+#ifndef ShowerConstants_h
+#define ShowerConstants_h
+
+/**
+ * ShowerConstants
+ * Defines constants for the shower simulation
+ */
+
+namespace ShowConst {
+    static const float e0     = 50;
+    static const float x0     = 1e-2;
+    static const float a      = 4.;
+    static const float b      = .5;
+    static const float molRad = 5e-2; // Moliere radius of the shower
+}
+
+#endif
