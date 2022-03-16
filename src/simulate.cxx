a1f1af9718610080d530ed7b42398fd4ae4dd606
gabouric <gabouric@gmail.com>
2022-03-16 17:20:31 +0100

    Implemented file

--- src/simulate.cxx
+++ src/simulate.cxx
@@ -1,8 +1,33 @@
 #include "Event.h"
+#include "CaloSimulation.h"
+#include "ShowerConstants.h"
+
+#include "TRandom.h"
 
 void simulate(Event& event)
 {
     // simulate the event
+    event.seteTrue(ShowConst::e0); // fixed true energy
 
-    event.seteTrue(100.); // fixed true energy
+    // Build calorimeter
+    // CAN DO: Add a caloReset method to empty all cells and define only one
+    // calorimeter for all the events.
+    CaloSimulation caloSim;
+    caloSim.CalorimeterData();
+
+    // Draw random point on calorimeter surface.
+    float x = gRandom->Uniform(abs(CalConst::XYMin - CalConst::XYMax))
+           + CalConst::XYMin;
+    float y = gRandom->Uniform(abs(CalConst::XYMin - CalConst::XYMax))
+           + CalConst::XYMin;
+
+    float impactPoint[2] = {x, y};
+
+    // Simulate the shower
+    caloSim.SimulateShower(x, y, ShowConst::e0);
+    CaloSimulation::CalData calData = caloSim.GetCalData();
+
+    // Store variables in event
+    event.setImpactPoint(impactPoint);
+    event.setCalData(calData);
 }
