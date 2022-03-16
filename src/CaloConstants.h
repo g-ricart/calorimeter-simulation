3928415fd29a8460cad3c87e2f3719e3f1b778a7
gabouric <gabouric@gmail.com>
2022-03-16 11:10:04 +0100

    alignement

--- src/CaloConstants.h
+++ src/CaloConstants.h
@@ -8,17 +8,17 @@
 
 namespace CalConst {
 	// Dimensions, in meters.
-	static const float ZMin = 0.;
-	static const float ZMax = 1.;
+	static const float ZMin  = 0.;
+	static const float ZMax  = 1.;
 	static const float XYMin = -2.;
 	static const float XYMax = 2.;
 
 	static const int NbCellsInXY = 40;
-	static const int NbLayers = 1;
+	static const int NbLayers    = 1;
 
 	// Cell size in x-y.
 	static const float XYSize = (XYMax - XYMin) / NbCellsInXY;
-	static const float ZSize = (ZMax - ZMin) / NbLayers;
+	static const float ZSize  = (ZMax - ZMin) / NbLayers;
 }
 
 #endif
