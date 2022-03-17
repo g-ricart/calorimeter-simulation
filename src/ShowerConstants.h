d64f979088fb02aba9de1dee562c431db6a8b578
npac07 <a@b>
2022-03-17 14:14:45 +0100

    documentation in ShowerConstants.h

--- src/ShowerConstants.h
+++ src/ShowerConstants.h
@@ -6,12 +6,17 @@
  * Defines constants for the shower simulation
  */
 
+    //! Initial energy of the particle e0.
+    //! Radiation length x0.
+    //! Variables of the dEdt fonction a and b.
+    //! Moliere radius of the shower molRad.
+
 namespace ShowConst {
     static const float e0     = 50;
     static const float x0     = 1e-2;
     static const float a      = 4.;
     static const float b      = .5;
-    static const float molRad = 5e-2; // Moliere radius of the shower
+    static const float molRad = 5e-2;
 }
 
 #endif
