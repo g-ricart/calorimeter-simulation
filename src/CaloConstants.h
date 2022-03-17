e306acc0777f9295f877d801d96c102918593e23
npac07 <a@b>
2022-03-17 14:10:56 +0100

    documentation in CaloConstants.h

--- src/CaloConstants.h
+++ src/CaloConstants.h
@@ -7,22 +7,27 @@
  */
 
  //! Dimensions of the calorimeter, in meters.
- /*! The front face of the
-     calorimeter is set to be the xy plane. We assume that it is a square
-     and the z axis represents the depth. */
+ //! Number of cells in the xy plane and number of layers.
+ //! Size of the cells in x, y and z.
+ /*! The front face of the calorimeter is set to be the xy plane.
+     We assume that it is a square and the z axis represents the depth.
+     */
 
 namespace CalConst {
 
-    static const float ZMin  = 0.; /*!< Enum minimal value of z axis */
-    static const float ZMax  = 1.; /*!< Enum maximal value of z axis */
-    static const float XYMin = -2.; /*!< Enum minimal value of x and y axis */
-    static const float XYMax = 2.; /*!< Enum maximal value of x and y axis */
+    // Dimensions of the calorimeter
+    static const float ZMin  = 0.;
+    static const float ZMax  = 1.;
+    static const float XYMin = -2.;
+    static const float XYMax = 2.;
 
-    static const int NbCellsInXY = 40; /*!< Enum number of cells in x (or y) direction */
-    static const int NbLayers    = 10; /*!< Enum number of layers in z direction */
+    // Number of cells
+    static const int NbCellsInXY = 40;
+    static const int NbLayers    = 10;
 
-    static const float XYSize = (XYMax - XYMin) / NbCellsInXY;/*!< Enum Cell size in x-y*/
-    static const float ZSize  = (ZMax - ZMin) / NbLayers;/*!< Enum Cell size in x-y*/
+    // Size of the cells
+    static const float XYSize = (XYMax - XYMin) / NbCellsInXY;
+    static const float ZSize  = (ZMax - ZMin) / NbLayers;
 }
 
 #endif
