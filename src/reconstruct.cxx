1d568e162a8aa650bb57f56e433023fe30a7174c
gabouric <gabouric@gmail.com>
2022-03-17 18:32:41 +0100

    continued implementation

--- src/reconstruct.cxx
+++ src/reconstruct.cxx
@@ -3,9 +3,11 @@
 #include "TRandom3.h"
 #include "TF1.h"
 
-#include "Event.h"
 #include "ShowerConstants.h"
+#include "Event.h"
 #include "CaloSimulation.h"
+#include "CaloCell.h"
+#include "CellAddress.h"
 
 using namespace std;
 
@@ -16,4 +18,26 @@ using namespace std;
 void reconstruct(Event& event, CaloSimulation& caloSim)
 {
     CaloSimulation::CalData calData = caloSim.GetCalData();
+
+    CellAddress currentAddress;
+    CaloCell    currentCell;
+
+    // List to store energies
+    float layerEnergies[CalConst::NbLayers] = {0.}; // List of layer energies
+    float xEnergies[CalConst::NbCellsInXY]  = {0.}; // List of energies along X
+    float yEnergies[CalConst::NbCellsInXY]  = {0.}; // List of energies along Y
+
+    for (auto& it: calData) {
+        currentAddress = it.first;
+        currentCell    = it.second;
+
+        float layer  = currentAddress.layer();
+        float ix     = currentAddress.ix();
+        float iy     = currentAddress.iy();
+        float energy = currentAddress.energyMeas()
+
+        layerEnergies[layer] += energy;
+        xEnergies[ix]        += energy;
+        yEnergies[iy]        += energy;
+    }
 }
