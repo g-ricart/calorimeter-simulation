dae48b903cfc20dc571bdc27d86df226792404b5
npac07 <a@b>
2022-03-17 11:52:28 +0100

    add documentation to CaloConstants.h

--- src/CaloConstants.h
+++ src/CaloConstants.h
@@ -7,18 +7,19 @@
  */
 
 namespace CalConst {
-    // Dimensions, in meters.
-    static const float ZMin  = 0.;
-    static const float ZMax  = 1.;
-    static const float XYMin = -2.;
-    static const float XYMax = 2.;
+    //! Dimensions of the calorimeter, in meters. The front face of the
+    //!calorimeter is set to be the xy plane. We assume that it is a square
+    //! and the z axis represents the depth.
+    static const float ZMin  = 0.; /*!< minimal value of z axis */
+    static const float ZMax  = 1.; /*!< maximal value of z axis */
+    static const float XYMin = -2.; /*!< minimal value of x and y axis */
+    static const float XYMax = 2.; /*!< maximal value of x and y axis */
 
-    static const int NbCellsInXY = 40;
-    static const int NbLayers    = 10;
+    static const int NbCellsInXY = 40; /*!< number of cells in x (or y) direction */
+    static const int NbLayers    = 10; /*!< number of layers in z direction */
 
-    // Cell size in x-y.
-    static const float XYSize = (XYMax - XYMin) / NbCellsInXY;
-    static const float ZSize  = (ZMax - ZMin) / NbLayers;
+    static const float XYSize = (XYMax - XYMin) / NbCellsInXY;/*!< Cell size in x-y*/
+    static const float ZSize  = (ZMax - ZMin) / NbLayers;/*!< Cell size in x-y*/
 }
 
 #endif
