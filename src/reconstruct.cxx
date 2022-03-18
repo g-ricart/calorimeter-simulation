cd42564cece01f7eea77109c907fb701bd608b29
gabouric <gabouric@gmail.com>
2022-03-18 23:51:34 +0100

    Implemented S-Shapr corrected reconstruction NOT WORKING AS INTENDED

--- src/reconstruct.cxx
+++ src/reconstruct.cxx
@@ -3,6 +3,7 @@
 #include "TRandom3.h"
 #include "TGraph.h"
 #include "TF1.h"
+#include "TMath.h"
 
 #include "ShowerConstants.h"
 #include "CaloConstants.h"
@@ -17,6 +18,12 @@ using namespace std;
  * @brief This function generates the reconstructed impact point.
  * @param[in] event Event object.
  */
+
+float sShape(float x){
+    return CalConst::s0 + CalConst::s1*x + CalConst::s2*pow(x, 2)
+                                         + CalConst::s3*pow(x, 3);
+}
+
 void reconstruct(Event& event,
                  TF1*   fitFunc,
                  float* xCenterArray,
@@ -37,9 +44,9 @@ void reconstruct(Event& event,
         currentAddress = it.first;
         currentCell    = it.second;
 
-        int layer  = currentAddress.layer();
-        int ix     = currentAddress.ix();
-        int iy     = currentAddress.iy();
+        int layer    = currentAddress.layer();
+        int ix       = currentAddress.ix();
+        int iy       = currentAddress.iy();
         float energy = currentCell.energyMeas();
 
         layerEnergies[layer] += energy;
@@ -61,23 +68,36 @@ void reconstruct(Event& event,
 
         float layerEnergy = layerEnergies[layer];
 
-        if (layerEnergy <= CalConst::layerEnrThr) {
+        if (layerEnergy >= CalConst::layerEnrThr) {
 
             // TGraphs to fit X and Y energies
             TGraph* graphX = new TGraph(CalConst::NbCellsInXY, xCenterArray, xEnergies);
             TGraph* graphY = new TGraph(CalConst::NbCellsInXY, yCenterArray, yEnergies);
 
-            graphX->Fit(fitFunc, "NOQ");
-            xReco += fitFunc->GetParameter("mean")*layerEnergy;
+            graphX->Fit(fitFunc, "Q");
+            xReco += fitFunc->GetParameter(1)*layerEnergy;
 
-            graphY->Fit(fitFunc, "NOQ");
-            yReco += fitFunc->GetParameter("mean")*layerEnergy;
+            // cout << "xReco : " << xReco << " ; " << event.xTrue() << endl;
+            // cout << "layerEnergy : " << layerEnergy << endl;
+
+            graphY->Fit(fitFunc, "Q");
+            yReco += fitFunc->GetParameter(1)*layerEnergy;
+
+            // cout << "yReco : " << yReco << " ; " << event.yTrue() << endl;
         }
 
+    }
     xReco /= event.eReco();
     yReco /= event.eReco();
 
+    xReco += sShape(int((xReco - CalConst::XYMin)/CalConst::XYSize)
+                                        *CalConst::XYSize/2. + CalConst::XYMin);
+    yReco += sShape(int((yReco - CalConst::XYMin)/CalConst::XYSize)
+                                        *CalConst::XYSize/2. + CalConst::XYMin);
+
     event.setxReco(xReco);
     event.setyReco(yReco);
-    }
+
+    // cout << "xReco : " << xReco;
+    // cout << "; xTrue : " << event.xTrue() << endl;
 }
